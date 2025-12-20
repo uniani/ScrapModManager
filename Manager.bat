8124f1cdb579c623e24a749b3af7d1389663fd27
WakeUpBro <107545998+uniani@users.noreply.github.com>
2025-12-20 12:31:00 +0300

    Auto game path define

--- Manager.bat
+++ Manager.bat
@@ -1,16 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 chcp 65001>nul
 title Scrap Mod Manager
 for /f "tokens=2,*" %%a in ('reg query "HKCU\SOFTWARE\Valve\Steam" /v "SteamPath"') do (
-set Steam_Path=%%b )
-set /p GAME_DIR="Enter game directory path: "
+set "Steam_Path=%%b" )
+if not defined Steam_Path (
+set /p GAME_DIR=Enter game path:
+) else (
+set "GAME_DIR=%Steam_Path%/steamapps/common/Scrap Mechanic"
+)
 call :write_cfg
 pause
 exit
 
 :write_cfg
 (
-echo GAME_DIR=%GAME_DIR%
-echo 
-)>Data/config.cfg
+echo GAME_DIR=!GAME_DIR!
+)>"Data/config.cfg"
 exit /b
